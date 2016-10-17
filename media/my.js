76a209be35f44970e94cda34354e06c194cf62d8
BEvgeny <o.velikij@gmail.com>
2016-10-17 21:51:48 +0300

    + уменьшил количество кода(всего 20 строк) + работают все функции - поддержка EVAL

--- media/my.js
+++ media/my.js
@@ -13,34 +13,8 @@ function foo(num) {
 	pin.value += num.value;
 	}
 }
- // внесена реализация "демо" калькулятора.
-
 function isSubmit() {
-var calc = {
-	'+' : function(a, b) { return a + b; },
-	'*' : function(a, b) { return a * b; },	
-	'-' : function(a, b) { return a - b; },
-	'/' : function(a, b) { return a / b; },
-};
-
-function calculate() {
-	var mark = array.join('');
-	var re = /^(\d+[,|.]?\d*)([+\-*\/])(\d+[,|.]?\d*)$/; //регулярное
-	if (mark = re.exec(mark)) {
-		var num1 = mark[1] * 1;
-		var num2 = mark[3] * 1;
-		var op = mark[2];
-		var result = calc[op](num1, num2);
-		pin.value = result;
-		}
-			if (result == Infinity) {
-			pin.value = "Так нельзя!"
-	}
-}
-calculate()
-console.log(array);
-document.getElementById('history').innerHTML += "Массив: [" + array + "] \n" 
-+ "Высчитать: " + array.join('') + "\n" + "Результат: " + pin.value + '\n' + '\n';
-
+pin.value = eval(array.join(''));
 array.splice(0,array.length,pin.value);
+document.getElementById('history').innerHTML += pin.value + '\n';
 }
